926c761ad5efc10f67354f83577971b2faabee3f
ColoMack <colomack6@gmail.com>
2023-12-31 14:27:33 +0300

    Commented files.

--- src/Login.js
+++ src/Login.js
@@ -1,39 +1,58 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './App.css'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
-import LoginInput from './components/LoginInput';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup'
 
+// the yup schema which is used in validating the input fields..here is where the errors are defined incase the validation is not met.
 const schema = yup.object({
     usernameEmail:yup.string().required("Username or Email is Required"),
-    password: yup.string().required("Password is Required"),
+    password: yup.string().required("Password is Required")
   });
 
 
 export default function Login() {
-    const navigate = useNavigate();
 
-    const {
-        handleSubmit,
-        register,
-        formState: {errors}
-    } = useForm({
-        resolver: yupResolver(schema)
-    });
+  // this is the validation logic that uses the schema that I made.
+  // handleSubmit does the work of verifying the form first for validation before submission
+  // register is for connecting the input fields to the form state
+  // formstate is for getting the current error state of the input field
+  const { 
 
-    const onSubmit = (data) => {
-        const userData = JSON.parse(localStorage.getItem(data.email));
-        if (userData) {
-            if (userData.password === data.password) {
-                navigate('/Dashboard')
-                console.log(userData.name + "You are successfully logged in")
-            } else {
-                console.log('Email or password is wrong')
-            }
-        }
-    };
+    handleSubmit, 
+    register, 
+    formState: {errors}
+
+  } = useForm({
+
+    // i've used yup library for managing the validation 
+    resolver: yupResolver(schema)
+
+  });
+
+//   will be used for routing to another page.
+  const navigate = useNavigate();
+
+//   getting the values of the input fields
+  const loginEmail = useRef();
+  const loginPassword = useRef();
+
+//   the login logic
+  const handleLogin = () => {
+    // check if the email and password values match with the stored credentials in local storage
+    if (loginEmail.current.value === localStorage.getItem('Email') && loginPassword.current.value === localStorage.getItem('Password')) {
+        // show the success message
+        alert('Login Successful')
+        // navigate the user to the dashboard page
+        navigate('/Dashboard')
+    } else {
+    // show the unsuccessful message
+        alert('Invalid Login Credentials')
+        // reload the page
+        window.location.reload();
+    }
+  }
 
     return (
     <div>
@@ -43,28 +62,24 @@ export default function Login() {
                 <span style={{color:'grey', fontSize:'80%', marginTop:'0.2rem'}}>Welcome back.</span>
             </div>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-            
-                <LoginInput 
-                    label='Username or Email'
-                    type='text'
-                    id='usernameEmail'
-                    placeholder='Enter Username or Email'
-                    register={{ ...register('usernameEmail') }}
-                    errorMessage={errors.usernameEmail?.message}
-                /> 
+            {/* the login form */}
+            <form onSubmit={handleSubmit(handleLogin)}>
+                {/* the input tags, each having the label and the error message */}
+                <div className='formInput'>
+                    <label htmlFor='email'>Email</label>
+                    <input type='text' id='email' placeholder='Email' ref={loginEmail} {...register}/>
+                    <span className='errorMsg'>{errors.loginEmail?.message}</span>    
+                </div>
 
-                <LoginInput 
-                    label='Password'
-                    type='password'
-                    id='password'
-                    placeholder='Enter New Password'
-                    register={{ ...register('password') }}
-                    errorMessage={errors.password?.message}
-                /> 
+                <div className='formInput'>
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' id='password' placeholder='Password' ref={loginPassword} {...register}/>
+                    <span className='errorMsg'>{errors.password?.message}</span>    
+                </div>
 
+                {/* the login button and the route to go to the sign up page */}
                 <div style={{display:'flex', flexDirection:'column', justifyContent:'center'}}>
-                    <input style={{padding:'0.4rem 0', borderRadius:'0.3rem', border:'none', backgroundColor:'blue', color:'white', fontWeight:'600', cursor:'pointer'}} type='submit' value='Login'/>
+                    <button style={{padding:'0.4rem 0', borderRadius:'0.3rem', border:'none', backgroundColor:'blue', color:'white', fontWeight:'600', cursor:'pointer'}} onClick={handleLogin}>Login</button>
                     <span style={{paddingTop:'0.4rem'}}>Already have an account? <Link to='/Signup' style={{textDecoration:'none', color:'blue'}}>Sign up</Link></span>
                 </div>
                 
--- src/Signup.js
+++ src/Signup.js
@@ -1,14 +1,13 @@
 import './App.css';
-import React from 'react';
-import Input from './components/Input';
+import React, { useRef} from 'react';
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup'
 
+// the schema that will be used in the valifdatiob logic.
 const schema = yup.object({
-  firstname:yup.string().required("Firstname is Required"),
-  lastname:yup.string().required("Lastname is Required"),
+  fullname:yup.string().required("Fullname is Required"),
   email:yup.string().required("Email is a required field").email("Email is not valid"),
   username:yup.string().required("Username is Required"),
   password: yup.string().min(6, "Password must be at least 6 characters"),
@@ -17,114 +16,108 @@ const schema = yup.object({
 
 function Signup() {
 
-  const navigate = useNavigate();
+  // this is the validation logic that uses the schema that I made.
+  // handleSubmit does the work of verifying the form first for validation before submission
+  // register is for connecting the input fields to the form state
+  // formstate is for getting the current error state of the input field
   const { 
+
     handleSubmit, 
     register, 
-    formState: {errors} 
+    formState: {errors}
+
   } = useForm({
+
+    // i've used yup library for managing the validation 
     resolver: yupResolver(schema)
+
   });
+  // will be used for navigating to another page
+  const navigate = useNavigate();
 
-  function formSubmit(data) {
-    data.preventDefault();
+  // capturing the values of the input components
+  const inputFullname = useRef();
+  const inputEmail = useRef();
+  const inputUsername = useRef();
+  const inputPassword = useRef();
 
-    try {
-      if (data) {
-        // check for existing user
-        const existingUser = localStorage.getItem(data.email);
-        if (existingUser) {
-          console.error(data.email, 'Already Exists!')
-          return;
-        }
+  // the sign up logic
+  const formSubmit = () => {
+    // check if values are null
+    if (inputFullname.current.value && inputEmail.current.value && inputUsername.current.value && inputPassword.current.value) {
+      // storing each value in the local storage
 
-        const userData = {
-          firstname: data.firstname,
-          lastname: data.lastname,
-          username: data.username,
-          password: data.password
-        }
+      localStorage.setItem('Fullname', inputFullname.current.value)
+      localStorage.setItem('Email', inputEmail.current.value)
+      localStorage.setItem('Username', inputUsername.current.value)
+      localStorage.setItem('Password', inputPassword.current.value)
+      localStorage.setItem('signUp', inputEmail.current.value)
+      
+      // show success message
+      alert('Successful sign up')
+      // route to the dashboard page
+      navigate('/Dashboard')
 
-        // simulate server side storage
-        localStorage.setItem(data.email, JSON.stringify(userData));
-
-        // navigate to dashboard page when successful..
-        navigate('/Dashboard')
-        console.log(JSON.parse(localStorage.getItem(data.email)));
-
-      } else {
-      // handle  any validation error
-      console.error('Invalid signup details:');
-      console.log('Invalid signup details:', data);
-      }
-    } catch (error) {
-      // storage errors
-      console.error('Signup error', error);
+    } else {
+      // show the unsuccessful message
+      alert('Invalid sign up details')
+      window.location.reload();
     }
-
-
-    
   }
 
   return (
+  <>
     <div className='App'>
       <div style={{display:'flex', flexDirection:'column', marginBottom:'1rem'}}>
         <span style={{fontWeight:'600', fontSize:'200%'}}>Sign up</span>
         <span style={{color:'grey', fontSize:'80%', marginTop:'0.2rem'}}>Enter the details below.</span>
       </div>
 
+      {/* the sign up form */}
       <form onSubmit={handleSubmit(formSubmit)}>
-        <Input 
-          label='Fullname'
-          type='text'
-          id='fullname'
-          placeholder='Enter Fullname'
-          register={{ ...register("fullname") }}
-          errorMessage={errors.fullname?.message}
-        /> 
+        {/* the input tags, each having their label and error message */}
+        {/* once validation is not met, then the message appears */}
 
-        <Input 
-          label='Email'
-          type='text'
-          id='email'
-          placeholder='Enter Email'
-          register={{ ...register("email")}}
-          errorMessage={errors.email?.message}
-        /> 
+        <div className="formInput">
+          <label htmlFor='fullname'>Fullname</label>
+          <input type='text' id='fullname' placeholder='Fullname' ref={inputFullname} {...register}/>
+          <span className='errorMsg'>{errors.fullname?.message}</span> 
+        </div>
 
-        <Input 
-          label='Username'
-          type='text'
-          id='username'
-          placeholder='Enter Username'
-          register={{ ...register("username")}}
-          errorMessage={errors.username?.message}
-        /> 
+        <div className="formInput">
+          <label htmlFor='email'>Email</label>
+          <input type='text' id='email' placeholder='Email' ref={inputEmail} {...register}/>
+          <span className='errorMsg'>{errors.email?.message}</span> 
+        </div>
 
-        <Input 
-          label='Password'
-          type='password'
-          id='password'
-          placeholder='Enter New Password'
-          register={{ ...register("password")}}
-          errorMessage={errors.password?.message}
-        /> 
+        <div className="formInput">
+          <label htmlFor='username'>Username</label>
+          <input type='text' id='username' placeholder='Username' ref={inputUsername} {...register}/>
+          <span className='errorMsg'>{errors.username?.message}</span> 
+        </div>
+        
+        <div className="formInput">
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' placeholder='Password' ref={inputPassword} {...register}/>
+          <span className='errorMsg'>{errors.password?.message}</span> 
+        </div>
 
-        <Input 
-          label='Confirm Password'
-          type='password'
-          id='confirmPassword'
-          placeholder='Confirm Password'
-          register={{ ...register("confirmPassword")}}
-          errorMessage={errors.confirmPassword?.message}
-        />
+        <div className="formInput">
+          <label htmlFor='confirmPassword'>Confirm Password</label>
+          <input type='password' id='confirmPassword' placeholder='Confirm Password' {...register}/>
+          <span className='errorMsg'>{errors.confirmPassword?.message}</span> 
+        </div>
+
+        {/* the submit button and login route */}
         <div style={{display:'flex', flexDirection:'column', justifyContent:'center'}}>
-          <input style={{padding:'0.4rem 0', borderRadius:'0.3rem', border:'none', backgroundColor:'blue', color:'white', fontWeight:'600', cursor:'pointer'}} onClick={formSubmit} type='submit' value='Sign up'/>
+          <button style={{padding:'0.4rem 0', borderRadius:'0.3rem', border:'none', backgroundColor:'blue', color:'white', fontWeight:'600', cursor:'pointer'}} onClick={formSubmit}>Sign up</button>
           <span style={{paddingTop:'0.4rem'}}>Already have an account? <Link to='/Login' style={{textDecoration:'none', color:'blue'}}>Login</Link></span>
         </div>
           
       </form>
-    </div>
+      
+    </div>      
+  </>
   );
 }
 
